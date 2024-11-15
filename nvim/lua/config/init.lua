38eb3c7623434f298bc8e0990ba5f3faefee8c0c
rajihawa <46829217+rajihawa@users.noreply.github.com>
2024-11-15 04:38:52 +0000

    pre reset

--- nvim/lua/config/init.lua
+++
@@ -1,9 +0,0 @@
-require("config.remap")
-require("config.lazy")
-
--- Set tab size
-vim.opt.tabstop = 4     -- Set the number of space characters inserted for a tab
-vim.opt.shiftwidth = 4  -- Set the number of spaces to use for each indentation level
-vim.opt.expandtab = false  -- Use spaces instead of tabs
-vim.opt.mouse = "" -- Disable mouse
-
